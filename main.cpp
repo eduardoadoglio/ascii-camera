d8acca604695baeeb941a1e75173f8d2e4dfd70d
Eduardo Adoglio <eduardoadoglio4@gmail.com>
2022-02-26 12:49:19 -0300

    Use AsciiImage on main

--- main.cpp
+++ main.cpp
@@ -4,26 +4,23 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <string> 
+#include "asciicamera.cpp"
 
 using namespace cv;
 
 
-Mat createBlackImage(int width, int height) {
-    return Mat::zeros(width,height,CV_8U);
-}
-
 int main() {
 
     Mat image;
-
     namedWindow("Display window");
-
     VideoCapture capture(0);
+    
+    AsciiCamera asciiCamera = AsciiCamera();
     
     while(capture.isOpened()) {
         capture >> image;
         int r, g, b;
-        Mat blackImage = createBlackImage(image.rows, image.cols);
+        asciiCamera.setFrameBackground(image.rows, image.cols);
         for (int i = 0; i < image.rows; ++i)
         {
             Vec3b* pixel = image.ptr<Vec3b>(i);
@@ -32,13 +29,14 @@ int main() {
                 r = pixel[j][2];
                 g = pixel[j][1];
                 b = pixel[j][0];
-                std::string rv = std::to_string(r);
+                double brightness = (r+g+b)/3;
                 if (i % 20 == 0 && j % 20 == 0) {
-                    putText(blackImage, rv ,Point(j, i), FONT_HERSHEY_COMPLEX_SMALL, 0.8, Scalar(255),1,8,false); 
+                    std::string asciiValue = asciiCamera.getAsciiValueForBrightness(brightness);
+                    asciiCamera.writeAsciiToFrame(asciiValue, Point(j, i));
                 }
             }
         }
-        imshow("Display window", blackImage);
+        imshow("Display window", asciiCamera.getCurrentFrame());
         
         int c = waitKey(10);
 
